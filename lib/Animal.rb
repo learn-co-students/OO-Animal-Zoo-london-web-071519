27926971ed9dedce308c04d49b17b43a68b89393
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-22 14:04:13 +0100

    Zoo OO complete

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,24 @@
+#can only be in one location at a time.
 class Animal
+    attr_accessor :weight, :location, :zoo
+    attr_reader  :species, :nickname
 
+    @@all = []
+    
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @location = nil
+        @zoo = nil
+        @@all << self
+    end
+
+    def self.find_by_species(species)
+        @@all.select {|animal| animal.species == species}
+    end
+
+    def self.all
+        @@all
+    end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,23 @@ require 'pry'
 
 #Test your code here
 
+columbia = Zoo.new("The Main Zoo", "Columbia, MD")
+eldersburg = Zoo.new("The Second Zoo", "Eldersburg, MD")
+arlington = Zoo.new("The Third Zoo", "Arlington, VA")
+another_in_columbia = Zoo.new("Second in Columbia", "Columbia, MD")
+
+
+jerry = Animal.new("Rat", 4, "Jerry")
+tom = Animal.new("Cat", 5, "Tom")
+tony = Animal.new("Cat", 260, "Tony")
+simba = Animal.new("Cat", 300, "Simba")
+zazu = Animal.new("Bird", 10, "Zazu")
+
+columbia.add_animal(tony)
+columbia.add_animal(simba)
+eldersburg.add_animal(jerry)
+eldersburg.add_animal(tom)
+arlington.add_animal(zazu)
 
 binding.pry
 puts "done"
