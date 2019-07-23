b1c116a41370b0217cf72b37f2bce92b369f3e90
Harry Chopra <harrychopra@gmail.com>
2019-07-23 16:36:51 +0100

    Solved

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,32 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo_name
+    attr_writer 
+    @@all = []
+
+    # An animal should be instantiated with the species (e.g. "Cat", "Dog", "Rat"), a numerical weight and a nickname. Keep in mind that the animal's species and nickname should not be able to change, but its weight can.
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo_name = zoo
+        @@all << self
+
+    end
+
+    # Animal.all should return an array of all the animal instances.
+    def self.all
+        @@all
+    end
+    
+    # Animal#zoo should return the zoo instance that the instance belongs to.
+    def zoo
+      Zoo.all.find{|zoo| zoo.name == @zoo_name}
+    end
+    
+    # Animal.find_by_species should take in an animal's species as an argument and return an array of all the animals, which are of that species.
+    def self.find_by_species(species)
+        @@all.select{|animal| animal.species == species}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -1,10 +1,33 @@
-require_relative "lib/Animal.rb"
-require_relative "lib/Zoo.rb"
 require 'pry'
+require_relative 'lib/animal'
+require_relative 'lib/zoo'
 
+# My seed data
+z1 = Zoo.new("ZSL Zoo", "London")
+z2 = Zoo.new("Battersea Zoo", "London")
+z3 = Zoo.new("Hanwell Zoo", "West London")
 
-#Test your code here
+a1 = Animal.new("Gorilla","150","Coco","ZSL Zoo")
+a2 = Animal.new("Okapi","250","Daisy","Battersea Zoo")
+a3 = Animal.new("Panda","85","Goofy","Battersea Zoo")
+a4 = Animal.new("Elephant","5800","Holly","ZSL Zoo")
+a5 = Animal.new("Indian Rhino","2100","Hutch","Hanwell Zoo")
+a6 = Animal.new("Jaguar","85","Jasper","Hanwell Zoo")
+a8 = Animal.new("Giraffe","800","Luca","ZSL Zoo")
+a9 = Animal.new("Chimpanzee","50","Maya","Hanwell Zoo")
+a10 = Animal.new("Elephant","5500","Wilbur","ZSL Zoo")
 
+# Methods i've tested to confirm my code against requirements
+Animal.all
+Zoo.all
+a1.weight = 160
+z1.animals
+z2.animals
+z1.animal_nicknames
+z1.animal_species 
+z1.find_by_species("Elephant")
+a1.zoo
+a2.zoo
+Zoo.find_by_location("London")
 
 binding.pry
-puts "done"
